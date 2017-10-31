de1f7b9bb08cf36249736a46eb19d6189a4986c3
SaulSandovalM <sauldevelop@gmail.com>
2017-10-31 15:05:27 -0600

    pull

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,8 @@ import {Router, Scene} from 'react-native-router-flux';
 import Login from './components/login/Login';
 import Log from './components/login/Log';
 import Recover from './components/login/Recover';
+import Detalle from './components/detalle/Detalle';
+import Detalle2 from './components/detalle/Detalle2';
 
 const Routes = () => {
     return (
@@ -13,6 +15,8 @@ const Routes = () => {
               <Scene key="Log" header={null} component={Log} initial/>
               <Scene key="Login" header={null} component={Login} />
               <Scene key="Recover" header={null} component={Recover} />
+              <Scene key="Detalle" header={null} component={Detalle} />
+              <Scene key="Detalle2" header={null} component={Detalle2} />
             </Scene>
           </Router>
         </Root>
--- src/components/detalle/Detalle.js
+++ src/components/detalle/Detalle.js
@@ -1,130 +1,144 @@
 import React, {Component} from 'react';
-import {Image, Text, ScrollView} from 'react-native';
-import {Container, Card, CardItem, Thumbnail, Button, Icon, Left, Body} from 'native-base';
+import {StyleSheet, Text, View, ScrollView, Image} from 'react-native';
+import {Container, List, ListItem, Thumbnail, Body, Right, Icon} from 'native-base';
+import {Actions} from 'react-native-router-flux';
 
-export default class Detalle extends Component < {} > {
-  render() {
-    return (
-      <Container>
+export default class Login extends Component < {} > {
+  render(){
+    return(
+      <Container style={styles.back}>
         <ScrollView>
-          <Card style={{flex: 0}}>
-            <CardItem>
-              <Left>
-                <Body>
-                  <Text>NativeBase</Text>
-                  <Text note>April 15, 2016</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <Body>
-                <Image source={{uri: 'Image URL'}} style={{height: 200, width: 200, flex: 1}}/>
-                <Text>
-                  //Your text here
-                </Text>
-              </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{color: '#87838B'}}>
-                  <Icon name="logo-github" />
-                  <Text>1,926 stars</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
+        <List>
+          <ListItem onPress={() => Actions.Detalle2()}>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656278_246443_20150615141713_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Enviado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656238_246443_20150615135424_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Entregado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_678439_246443_20150703132445_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Enviado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656297_246443_20150615143234_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Recibido</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656278_246443_20150615141713_b.jpg'}} />
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Enviado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656278_246443_20150615141713_b.jpg'}} />
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Entregado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656278_246443_20150615141713_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Enviado</Text>
+            </Right>
+          </ListItem>
+
+          <ListItem>
+            <Thumbnail
+              style={styles.img}
+              source={{
+                uri: 'https://s3.amazonaws.com/kichink/items_656278_246443_20150615141713_b.jpg'}}/>
+            <Body style={styles.margin}>
+              <Text>Pedido 1</Text>
+              <Text note>Mar 31 de Oct</Text>
+            </Body>
+            <Right>
+              <Text>Status: Enviado</Text>
+            </Right>
+          </ListItem>
+        </List>
         </ScrollView>
       </Container>
     );
-  }
-}
+  };
+};
 
 const styles = StyleSheet.create({
-    video: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0
-    },
-    img: {
-        justifyContent: 'flex-end',
-        flex: 2,
-        height: null,
-        width: null
-    },
-    texto: {
-        color: 'black',
-        alignSelf: 'center',
-        fontWeight: 'bold',
-        marginTop: 20,
-        backgroundColor: 'transparent'
-    },
-    view: {
-        margin: 15
-    },
-    view2: {
-        justifyContent: 'flex-end',
-        backgroundColor: 'transparent'
-    },
-    view3: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        marginBottom: 10
-    },
-    view4: {
-        flex: 1,
-        margin: 20
-    },
-    inputRounded: {
-        marginRight: 40,
-        marginLeft: 40,
-        marginBottom: 10,
-        borderColor: 'orange',
-        borderWidth: 1.5,
-        backgroundColor: 'rgba(0,0,0,.5)'
-    },
-    boton: {
-        color: 'white',
-        fontWeight: 'bold',
-        alignSelf:'center',
-    },
-    text: {
-        color: 'white'
-    },
-    text1: {
-        color: 'white',
-        fontSize: 18
-    },
-    adelante: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        margin: 20
-    },
-    button: {
-        width: '78%',
-        justifyContent: 'center',
-        alignSelf:'center',
-        backgroundColor: 'orange',
-    },
-    icon: {
-        backgroundColor: 'transparent',
-        color: 'orange',
-        fontSize: 50
-    },
-    buttonSpinner: {
-        marginRight: 140,
-        marginLeft: 140,
-        marginBottom: 10,
-        backgroundColor: 'orange'
-    },
-    color: {
-        color: 'white'
-    },
-    errorText: {
-        fontSize: 20,
-        alignSelf: 'center',
-        color: 'red',
-    }
+  back: {
+    backgroundColor: 'white'
+  },
+  img: {
+    width: 100,
+    height: 100
+  },
+  margin: {
+    marginLeft: 10
+  },
+  margin2: {
+    marginRight: 15
+  }
 });
