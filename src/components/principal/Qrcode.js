713e6b8c0f5b41ed48e9fb017c22a6e21ceb8d20
Dylan <dyl.blink.1.8.2@gmail.com>
2017-12-19 19:11:25 -0600

    QR

--- src/components/principal/Qrcode.js
+++ src/components/principal/Qrcode.js
@@ -1,10 +1,55 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
 
-export default class Qrcode extends Component{
-    render(){
-        return(
-            <Text>Dylan Code</Text>
-        )
-    }
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+  Image
+} from 'react-native';
+import Camera from 'react-native-camera';
+import {Cabecera} from "../orden/Cabecera";
+import {Actions} from 'react-native-router-flux';
+
+export default class Camara extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Cabecera headerText={"LECTOR"} onPress={()=>Actions.pop()} />
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}>
+
+
+
+              <Image style={styles.capture} source={{uri:'https://image.flaticon.com/icons/png/512/19/19862.png'}}/>
+
+
+        </Camera>
+
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+  preview: {
+    flex:2,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  capture: {
+    height:200,
+    width:200,
+  },
+  
+});
